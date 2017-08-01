1f4851b5c137145beb6ece7f20abf067e112c476
Haziq Jamil <h.jamil@lse.ac.uk>
2017-08-01 21:32:50 +0100

    Finished draft 1

--- R/06-iprobit.R
+++ R/06-iprobit.R
@@ -1,16 +1,26 @@
 source("01-prelim.R")
 
-dat <- gen_circle(n = 750, m = 3, sd = 0.08)  # generate 3-class toy example data set
+dat <- gen_circle(n = 750, m = 3, sd = 0.09)  # generate 3-class toy example data set
 plot(dat)
-(mod <- iprobit(y ~ X1 + X2, dat, kernel = "FBM", control = list(maxit = 500)))
-
-dat.true <- gen_circle(n = 600, m = 3, sd = 0)
-ggplot(as.data.frame(dat.true)) + geom_path(aes(x = X1, y = X2, group = y, col = y))
-
+(mod <- iprobit(y ~ X1 + X2, dat, kernel = "FBM", control = list(maxit = 1500)))
 p <- iplot_predict(mod)
-p + geom_path(data = as.data.frame(dat.true), aes(x = X1, y = X2, group = y, col = y))
 
-dat.new <- gen_circle(n = 15, m = 3, sd = 0.08)
+# dat.true <- gen_circle(n = 600, m = 3, sd = 0)
+# ggplot(as.data.frame(dat.true)) + geom_path(aes(x = X1, y = X2, group = y, col = y))
+# p + geom_path(data = as.data.frame(dat.true), aes(x = X1, y = X2, group = y, col = y))
+
+dat.new <- gen_circle(n = 15, m = 3, sd = 0.09)
 dat.new.pred <- predict(mod, dat.new)
-p + geom_label(data = as.data.frame(dat.new), aes(x = X1, y = X2, col = dat.new.pred$y,
-                                                  label = apply(dat.new.pred$prob, 1, max)))
+dat.new.pred$prob[, dat.new$y]
+prob.lab <- diag(apply(dat.new.pred$prob, 1, function(x) decPlac(x[dat.new$y], 2)))
+p1 <- p +
+  geom_label(data = as.data.frame(dat.new),
+               aes(x = X1, y = X2, col = dat.new$y, label = prob.lab),
+               size = 3.3, show.legend = FALSE, label.size = 0.9) +
+  scale_x_continuous(breaks = NULL, expression(italic(X[1]))) +
+  scale_y_continuous(breaks = NULL, name = expression(italic(X[2]))) +
+  theme(legend.justification = c(1, 0), legend.position = c(1 - 0.015, 0 + 0.02),
+        legend.text = element_text(size = 10),
+        legend.background = element_rect(size = 0.5, colour = "grey10")) +
+  guides(colour = guide_legend(override.aes = list(size = 2.5))); p1
+ggsave("../figure/iprobit_multiclass.pdf", p1, width = 3.5 * 18 / 11, height = 3.5)
